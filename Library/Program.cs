c679b950b2e3ef85f7801c683cec9d30d30a0132
Jason Hutton <Hutton-J10@ulster.ac.uk>
2025-10-06 15:15:51 +0100

    Added a constructor to the Book class.

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Library
+{
+    class Book
+    {
+        string Title;
+        string Author;
+        string ISBN;
+
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            Title = bookTitle;
+            Author = bookAuthor;
+            ISBN = bookISBN;
+        }
+
+        void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Book book = new Book("C# for beginners", "Bill Gates", "1234567");
+
+            book.DisplayInfo();
+        }
+    }
+}
